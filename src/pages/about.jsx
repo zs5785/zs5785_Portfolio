8382989d134c81572d8e2540a33ff028c65f6be1
zs5785 <60033493+zs5785@users.noreply.github.com>
2026-07-05 22:09:48 -0700

    Updated Post Descriptions and About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -22,7 +22,7 @@ function Overview(){
     return (
         <div>
             <h2>About Me</h2>
-            <p>My name is Tong Yu Hei, but you can call me John. I recently graduated from City University of Hong Kong with a major in Creative Media. I was born in Hong Kong and currently studying Computer Studies at Langara College in Canada.</p>
+            <p>My name is Tong Yu Hei, but you can call me John. I am a graduate from City University of Hong Kong with a major in Creative Media. I was born in Hong Kong and currently studying Computer Studies at Langara College in Canada.</p>
             <p>I specialize in game design and programming. I have worked on multiple game projects as a team leader and programmer. I have also worked on some solo projects. I have four years of experience in Unity and game development. Currently, I am working on a game project in Unreal. I mainly focus on creating gaming experiences with high replayability and customization.</p>
             <h2>Educations</h2>
             <ul>
@@ -109,10 +109,6 @@ function Contact(){
                 <tr>
                     <td>Whatsapp</td>
                     <td>+1(236)660-2478</td>
-                </tr>
-                <tr>
-                    <td>Linkedin</td>
-                    <td><a href="https://linkedin.com/in/yu-hei-tong" target="_blank">linkedin.com/in/yu-hei-tong</a></td>
                 </tr>
                 <tr>
                     <td>Github</td>
